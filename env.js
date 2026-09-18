506c381bd89c001c980ecacbd0cde9edc6bc548d
idinasdf <nitirujidin@gmail.com>
2026-09-18 22:28:51 +0700

    Update env.js

--- env.js
+++ env.js
@@ -1,10 +1,6 @@
 // ไฟล์นี้ถูกสร้างอัตโนมัติจาก .env โดย generate-env.js
 // อย่าแก้ไขตรงนี้ / อย่า commit ขึ้น git
-
-const supabaseUrl = process.env.SUPABASE_URL || "ค่าเริ่มต้นกรณีไม่มีใน env";
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "ค่าเริ่มต้นกรณีไม่มีใน env";
-
 window.__ENV__ = {
-  SUPABASE_URL: supabaseUrl ,
-  SUPABASE_ANON_KEY: supabaseAnonKey
+  SUPABASE_URL: "https://ywvlbaahjxdjwpeetbyy.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_lPmrqIlhBX5yryL3AiGMuA_bAR1yqTi"
 };
